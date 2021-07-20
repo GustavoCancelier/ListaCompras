f1440cecd64755ef772d0e88ef0ca39e17b225a6
Gustavo Cancelier <gustavocansantos@gmail.com>
2021-07-20 17:15:42 -0300

    Atividade lista de compras

--- ListaCompra.java
+++ ListaCompra.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class ListaCompra {
     public static void main(String[] args) {
         List<String> produtos = new ArrayList<>();
-        Scanner ler = new Scanner(System.in);
-        while (produtos.size() < 5) {
-            System.out.println("Informe o que deseja adicionar na lista de compras: ");
-            produtos.add(ler.nextLine());
-        }
-        System.out.println("Produtos na lista de compras: ");
-        for (String itens : produtos) {
-            System.out.println(itens);
+        try (Scanner ler = new Scanner(System.in)) {
+            while (produtos.size() < 5) {
+                System.out.println("Informe o que deseja adicionar na lista de compras: ");
+                produtos.add(ler.nextLine());
+            }
+            System.out.println("Produtos na lista de compras: ");
+            for (String itens : produtos) {
+                System.out.println(itens);
+            }
         }
     }
 }
